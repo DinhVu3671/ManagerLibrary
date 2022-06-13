ffdcaa1a6328cf38ad8ac9e2aa4f2ea9e53404d4
Đinh Tiến Vũ <vudt2@sapo.vn>
2022-06-13 17:32:46 +0700

    config router

---
+++ backend/router/Books.js
@@ -0,0 +1,43 @@
+const booksController = require("../controllers/Books");
+const {asyncWrapper} = require("../utils/asyncWrapper");
+const express = require("express");
+const booksRoutes = express.Router();
+const auth = require("../middleware/auth");
+
+// //tạo
+// booksRoutes.post(
+//     "/create",
+//     auth,
+//     asyncWrapper(booksController.create)
+// );
+// // sửa
+// booksRoutes.post(
+//     "/edit/:id",
+//     auth,
+//     asyncWrapper(booksController.edit)
+// );
+// // lấy ra 1
+// booksRoutes.get(
+//     "/show/:id",
+//     auth,
+//     asyncWrapper(booksController.show),
+// );
+// // xoá
+// booksRoutes.get(
+//     "/delete/:id",
+//     auth,
+//     asyncWrapper(booksController.delete),
+// );
+// // lấy ra list
+// booksRoutes.get(
+//     "/list",
+//     auth,
+//     asyncWrapper(booksController.list),
+// );
+// booksRoutes.post(
+//     "/search",
+//     auth,
+//     booksController.searchUser);
+
+
+module.exports = booksRoutes;
---
+++ backend/router/Categories.js
@@ -0,0 +1,23 @@
+const categoriesController = require("../controllers/Categories");
+const {asyncWrapper} = require("../utils/asyncWrapper");
+const express = require("express");
+const categoriesRoutes = express.Router();
+const auth = require("../middleware/auth");
+
+// categoriesRoutes.post(
+//     "/create",
+//     asyncWrapper(categoriesController.create)
+// );
+// categoriesRoutes.get(
+//     "/show",
+//     auth,
+//     asyncWrapper(categoriesController.show),
+// );
+
+// categoriesRoutes.post(
+//     "/search", 
+//     auth, 
+//     categoriesController.searchCategory
+// );
+
+module.exports = categoriesRoutes;

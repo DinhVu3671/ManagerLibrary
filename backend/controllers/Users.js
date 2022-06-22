5b35b536ec21c74d0f13b5280d6772c2241c2313
nguyenminhduc0910 <nguyenminhduc910200002@gmail.com>
2022-06-22 21:38:56 +0700

    update API get infor user

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -167,3 +167,23 @@ usersController.edit = async (req, res, next) => {
         });
     }
 }
+usersController.show = async (req, res, next) => {
+    try {
+        let userId = null;
+        if (req.params.id) {
+            userId = req.params.id;
+        } else {
+            userId = req.userId;
+        }
+        let user = await UserModel.findById(userId).select('fullName phone username gmail');
+        if (user == null) {
+            return res.status(httpStatus.NOT_FOUND).json({message: "Can not find user"});
+        }
+        return res.status(httpStatus.OK).json({
+            data: user
+        });
+    } catch (error) {
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({message: error.message});
+    }
+}
+module.exports = usersController;
